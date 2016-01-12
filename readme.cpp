1d0b5b44e14fc166407d75d21ad57b1a98f1a898
uelva <me@uelva.com>
2016-01-12 00:54:38 -0600

    just a test

---
+++ readme.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+
+void preprompt(){
+    cout<<"fuck you"<<endl;
+}
+
+int main(){
+
+preprompt();
+
+return 0;
+}
